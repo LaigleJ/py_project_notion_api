f9eaa19ffc9e75a750e07250d4233b02714eb4ae
LaigleJ <you@example.com>
2025-06-12 15:14:08 +0200

    par ville et par école

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # # main.py
 import os
 from pprint import pprint
-from notion_api import query_unbilled_entries,  get_database_properties
+from notion_api import extraire_interventions, query_unbilled_entries,  get_database_properties
 
 import pandas as pd
 
@@ -12,7 +12,7 @@ import pandas as pd
 
 # pour tester la query recuperer les datat facturé
 date_debut = "2025-01-01"
-date_fin = "2025-02-15"
+date_fin = "2025-12-31"
 facture = False
 
 results = query_unbilled_entries(date_debut, date_fin, facture)
@@ -24,7 +24,6 @@ print(f"✅ {len(results)} interventions récupérées.")
 # for i, row in enumerate(results[:3]):
 #     pprint(row)
 
-# Faire le csv
-# results = query_unbilled_entries(date_debut, date_fin, facture)
-# df = extraire_interventions(results)
-# print(df.head())
+# efxtraire interventions dans un DataFrame
+df = extraire_interventions(results)
+print(df.head())
